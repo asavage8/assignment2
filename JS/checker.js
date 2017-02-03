7237710c816a48406e36cf696f9d5285dc5831ea
asavage8 <ams467@pitt.edu>
2017-02-03 08:22:20 -0500

    Done?

--- JS/checker.js
+++ JS/checker.js
@@ -8,64 +8,103 @@
 
       catNode.addEventListener("change", chkCatName, false);
       catColorNode.addEventListener("change", chkCatColor, false);
-      catSizeNode.addEventListener("change", chkCatName, false);
+      catSizeNode.addEventListener("change", chkCatSize, false);
       ownerNameNode.addEventListener("change", chkOwnerName, false);
       ownerPhoneNode.addEventListener("change", chkOwnerPhone, false);
-      submitNode.addEventListener("click",submitForm , false);
-
-      var form = document.getElementById("myForm");
-      form.reset();
+      submitNode.addEventListener("click",screenOff , false);
 
 //----------------------------------------------------------------------------------------------------------
       function chkCatName(event) {
-
+        var regex = /^[a-zA-Z]*$/;
         var myName = event.currentTarget;
-        var pos = myName.value.match("^[a-zA-Z]+$");
-        if (pos = null) {
+        if (!regex.test(myName.value)) {
           alert("The name you entered " + myName.value +
                 " is not in the correct form. \n"+
-                "Please enter letters only\n" );
+                "Please enter letters only\n");
          }
       }
 
       function chkCatColor(event){
+        var regex = /^[a-zA-Z]*$/;
         var myColor = event.currentTarget;
-        var pos = myColor.value.match("^[a-zA-Z]+$");
-
-        if (pos != null) {
+        if (!regex.test(myColor.value)) {
           alert("The color you entered " + myColor.value +
                 " is not in the correct form. \n" );
          }
       }
 
       function chkCatSize(event){
-        var mySize = event.currentTarget;
-        var pos = mySize.value.search(/^[A-Z][a-z]\.?$/);
+        var mySize = event.currentTarget.value;
+        if(!(mySize == "Petite" || mySize == "petite" ||
+            mySize =="Regular" ||  mySize == "regular" ||
+            mySize == "Fat" || mySize == "fat")){
+          alert("The size you entered " + mySize +
+                " is not in the correct form. \n "+
+                " Please enter (Petite, Regular, Fat)\n"+ mySize);
+        }
       }
 
       function chkOwnerName(event){
+        var regex = /^[-a-zA-Z_.]* [-a-zA-Z_.]*$/;
         var myOwnerName = event.currentTarget;
-        var pos = myOwnerName.value.search(/^[A-Z][a-z], ?[A-Z]\.?$/);
-        if (pos = null) {
-          alert("The name you entered " + myName.value +
+          if (!regex.test(myOwnerName.value)) {
+          alert("The name you entered " + myOwnerName.value +
                 " is not in the correct form. \n "+
-                " Please enter (First Middle Last)\n" );
+                " Please enter (First Last)\n" );
          }
       }
 
       function chkOwnerPhone(event) {
+        var regex = /^\d{3}\-\d{3}\-\d{4}$/
         var myPhone = event.currentTarget;
-        var pos = myPhone.value.match(/^\d{3}-\d{3}-\d{4}$/);
-
-        if (pos = null) {
-          alert("The phone number you entered (" + myPhone.value +
-                ") is not in the correct form. \n" +
+        if (!regex.test(myPhone.value)) {
+          alert("The phone number you entered " + myPhone.value +
+                " is not in the correct form. \n" +
                 "The correct form is: ddd-ddd-dddd \n" +
                 "Please go back and fix your phone number");
         }
       }
 
-      function submitForm(event){
-        alert("Congrates your cat is the cutest!!!!");
-        document.getElementById("myForm").style.visibility = 'hidden';
+      function submitForm(){
+        var a = /^[a-zA-Z]*$/.test(document.getElementById("catName").value)
+        var b = /^[a-zA-Z]*$/.test(document.getElementById("catColor").value);
+        var c = document.getElementById("catSize").value;
+        var d = /^[-a-zA-Z_.]* [-a-zA-Z_.]*$/.test(document.getElementById("ownerName").value);
+        var e = /^\d{3}\-\d{3}\-\d{4}$/.test(document.getElementById("ownerPhone").value);
+
+        if(!(c == "Petite" || c == "petite" || c =="Regular" ||
+            c == "regular" || c == "Fat" || c == "fat")){
+              c=1;
+            }
+
+        if(!a){
+         alert("Cat Name Error: Format Incorrect");
+         return false;
+        } else if(!b){
+         alert("Cat Color Error: Format Incorrect");
+         return false;
+        } else if(!d){
+         alert("Owner Name Error: Format Incorrect");
+         return false;
+       } else if(!c){
+          alert("Cat Size Error: Use Exact Format");
+          return false;
+        }else if(!e){
+         alert("Owner Phone Error: Format Incorrect");
+         return false;
+        }else{
+         alert("Congrates your cat is the cutest!!!!\n"+
+               "We will be in conact with your photoshoot.");
+        return true;
+        }
+      }
+
+
+      function screenOff(event){
+        var decide = submitForm();
+        if(!decide){
+          event.preventDefaullt();
+        }else{
+          document.getElementById("myForm").style.visibility = 'hidden';
+        }
       }
